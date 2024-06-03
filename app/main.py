adb2d3210d837945feacb73a70dd09c1d5aa9a33
Utsav2931 <utsav@codeforfun.com>
2024-06-03 14:10:30 -0700

    Implemented REPL.

--- app/main.py
+++ app/main.py
@@ -5,13 +5,12 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     # print("Logs from your program will appear here!")
 
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
-
-    # Wait for user input
-    command = input()
-    print(f"{command}: command not found")
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+        # Wait for user input
+        command = input()
+        print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
