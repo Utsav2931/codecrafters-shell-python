f688fa6d0045862589da7c19ee588a89c5c41e68
Utsav2931 <utsav@codeforfun.com>
2024-06-09 23:48:20 -0600

    Added type command.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import sys
 
-
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     # print("Logs from your program will appear here!")
@@ -9,21 +8,30 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
         # Wait for user input
-        command = input().split(" ")
-        print(execute_command(command))
+        user_command = input().split(" ")
+        print(execute_command(user_command))
 
 
-def execute_command(command):
+def execute_command(user_command):
     res = ""
-    match command[0]:
+    match user_command[0]:
         case "exit":
-            exit(int(command[1]))
+            exit(int(user_command[1]))
         case "echo":
-            res = " ".join(command[1:])
+            res = " ".join(user_command[1:])
+        case "type":
+            res = type(user_command[1])
         case _:
-            res = f"{command[0]}: command not found"
+            res = f"{user_command[0]}: command not found"
 
     return res
+
+def type (command):
+    commands = {"echo", "type", "exit"}
+    if command in commands:
+        return f"{command} is a shell builtin"
+    else:
+        return f"{command} not found"
 
 if __name__ == "__main__":
     main()
