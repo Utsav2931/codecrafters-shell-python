8c44f1143cbc559282894581b233c611df302a62
Utsav2931 <utsav@codeforfun.com>
2024-06-11 15:23:05 -0700

    Added support for executing commands.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from os.path import isfile
+import subprocess
 import sys
 import os
 
@@ -26,6 +26,14 @@ def execute_command(user_command):
         case "type":
             res = type(user_command[1])
         case _:
+            file_path = get_file_path(user_command[0])
+            if file_path != "":
+                result = subprocess.run(
+                    user_command,
+                    capture_output = True,
+                    text = True,
+                )
+                return result.stdout.strip("\n")
             res = f"{user_command[0]}: command not found"
 
     return res
@@ -35,11 +43,17 @@ def type (command):
     if command in shell_builtins:
         return f"{command} is a shell builtin"
     else:
+        file_path = get_file_path(command)
+        if file_path != "":
+            return f"{command} is {file_path}"
+        return f"{command}: not found"
+
+def get_file_path (file_name):
         paths = PATH.split(":")
         for path in paths:
-            if os.path.isfile(f"{path}/{command}"):
-                return f"{command} is {path}/{command}"
-        return f"{command} not found"
+            if os.path.isfile(f"{path}/{file_name}"):
+                return f"{path}/{file_name}"
+        return ""
 
 if __name__ == "__main__":
     main()
